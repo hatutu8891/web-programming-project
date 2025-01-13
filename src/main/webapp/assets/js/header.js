4812fc0e467a95761642946e8c3e681447d56bf0
Tu Chi Ha <22130065@st.hcmuaf.edu.vn>
2025-01-13 23:30:26 +0700

    Thêm header.js vào để sử lý dữ liệu đươc người dùng thêm vào ô tìm kiếm

--- src/main/webapp/assets/js/header.js
+++ src/main/webapp/assets/js/header.js
@@ -1,11 +1,3 @@
-document.addEventListener("DOMContentLoaded", function() {
-    fetch('../../pages/dynamic/shared/header.jsp')
-        .then(response => response.text())
-        .then(data => {
-            document.getElementById('header').innerHTML = data;
-        })
-        .catch(error => console.error('Error fetching header:', error));
-});
 function search() {
     var query = document.getElementById("searchQuery").value;
     if(query) {
