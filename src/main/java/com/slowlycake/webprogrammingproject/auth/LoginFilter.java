7362e440a009a0f8f4efadb57ad3b670dc07514f
Le Ba Khanh Duy <22130058@st.hcmuaf.edu.vn>
2025-02-03 23:14:25 +0700

    fix login filter for admin pages

--- src/main/java/com/slowlycake/webprogrammingproject/auth/LoginFilter.java
+++ src/main/java/com/slowlycake/webprogrammingproject/auth/LoginFilter.java
@@ -4,33 +4,37 @@ import jakarta.servlet.*;
 import jakarta.servlet.annotation.WebFilter;
 import jakarta.servlet.http.*;
 import java.io.IOException;
-import java.util.*;
 
-@WebFilter("/adminDashboard.jsp")
+@WebFilter({"/adminDashboard.jsp", "/contactManagement.jsp", "/mailSubManagement.jsp",
+        "/orderManagement.jsp", "/userManagement.jsp", "/productManagement.jsp",
+        "/reviewManagement.jsp"})
 public class LoginFilter implements Filter {
-    private static final List<String> restrictedPages = Arrays.asList(
-            "/adminDashboard.jsp", "/contactManagement.jsp", "mailSubManagement.jsp", "/orderManagement.jsp",
-            "/userManagement.jsp", "/productManagement.jsp", "/reviewManagement.jsp"
-    );
 
     @Override
-    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
         HttpServletRequest req = (HttpServletRequest) request;
         HttpServletResponse resp = (HttpServletResponse) response;
 
         HttpSession session = req.getSession(false);
-        Object authUser = (session != null) ? session.getAttribute("auth") : null;
+        User authUser = (session != null) ? (User) session.getAttribute("auth") : null;
 
         if (authUser == null) {
-            req.setAttribute("error", "Vui lòng đăng nhập trước!");
-            req.getRequestDispatcher("/login.jsp").forward(req, resp);
-        } else {
-            chain.doFilter(request, response);
+            session = req.getSession(true); // Ensure session exists
+            session.setAttribute("error", "Vui lòng đăng nhập trước!");
+            resp.sendRedirect(req.getContextPath() + "/login.jsp");
+            return;
         }
+
+        if (authUser.getRole() == 1) {
+            chain.doFilter(request, response);
+            return;
+        }
+
+        resp.sendRedirect(req.getContextPath() + "/index.jsp");
     }
 
     @Override
     public void destroy() {
     }
 }
-
