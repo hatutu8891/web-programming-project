42e853742a49416ed118ccd935951c813f5061a6
Tu Chi Ha <22130065@st.hcmuaf.edu.vn>
2025-02-04 23:38:03 +0700

    Thêm truy vấn dư liệu về giá và loại bánh

--- src/main/java/com/slowlycake/webprogrammingproject/products/ProductDao.java
+++ src/main/java/com/slowlycake/webprogrammingproject/products/ProductDao.java
@@ -40,13 +40,14 @@ public class ProductDao {
         );
     }
 
-//    public List<Product> findProductsByPrice(int minPrice, int maxPrice ) {
-//        return JDBIConnect.get().withHandle(handle ->
-//                handle.createQuery("SELECT * FROM products p JWHERE category = :category")
-//                        .bind("category", category)
-//                        .mapToBean(Product.class)
-//                        .list()
-//        );
-//    }
+    public List<Product> findProductsByPrice(int minPrice, int maxPrice ) {
+        return JDBIConnect.get().withHandle(handle ->
+                (List<Product>) handle.createQuery("SELECT * FROM products p JOIN product_variants pv ON p.id= pv.pId JOIN variants v ON pv.vId= v.id WHERE v.price BETWEEN :minPrice AND :maxPrice")
+                .bind("minPrice", minPrice)
+                .bind("maxPrice", maxPrice)
+                .mapTo(Product.class)
+                .list()
+        );
+    }
 
 }
